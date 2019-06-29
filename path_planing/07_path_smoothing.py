0c57c97844133a5da8e2a91f35ea8a2d9dc53f55
anner-wang <741679043@qq.com>
2019-06-29 10:33:42 +0800

    路径平滑

---
+++ path_planing/07_path_smoothing.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+# User Instructions
+#
+# Define a function smooth that takes a path as its input
+# (with optional parameters for weight_data, weight_smooth,
+# and tolerance) and returns a smooth path. The first and 
+# last points should remain unchanged.
+#
+# Smoothing should be implemented by iteratively updating
+# each entry in newpath until some desired level of accuracy
+# is reached. The update should be done according to the
+# gradient descent equations given in the instructor's note
+# below (the equations given in the video are not quite 
+# correct).
+"""
+ 
+from copy import deepcopy
+import numpy as np
+import matplotlib.pyplot as plt
+
+# thank you to EnTerr for posting this on our discussion forum
+def printpaths(path,newpath):
+    for old,new in zip(path,newpath):
+        print('['+ ', '.join('%.3f'%x for x in old) + \
+             '] -> ['+ ', '.join('%.3f'%x for x in new) +']')
+
+# Don't modify path inside your function.
+path = [[0, 0],
+        [0, 1],
+        [0, 2],
+        [1, 2],
+        [2, 2],
+        [3, 2],
+        [4, 2],
+        [4, 3],
+        [4, 4]]
+
+
+def smooth(path, weight_data = 0.3, weight_smooth = 0.5, tolerance = 0.000001):
+
+    # Make a deep copy of path into newpath
+    newpath = deepcopy(path)
+
+    change = tolerance
+    while change >= tolerance:
+        change = 0
+        for i in range(1, len(path) - 1):
+            for j in range(len(path[0])):
+                aux = newpath[i][j]
+                # newpath[i][j] += weight_data * (path[i][j] - newpath[i][j])
+                # newpath[i][j] += weight_smooth * (newpath[i-1][j] + newpath[i+1][j] - (2 * newpath[i][j]))
+                newpath[i][j] += weight_data * (path[i][j] - newpath[i][j]) + \
+                    weight_smooth * (newpath[i-1][j] + newpath[i+1][j] - 2.0 * newpath[i][j])
+                change += abs(aux - newpath[i][j])
+    return newpath
+
+newpath = smooth(path)
+printpaths(path, newpath)
+
+x_trajectory_path = []
+y_trajectory_path = []
+x_trajectory_newpath = []
+y_trajectory_newpath = []
+for i in range(len(path)):
+    x_trajectory_path.append(path[i][0])
+    y_trajectory_path.append(path[i][1])
+    x_trajectory_newpath.append(newpath[i][0])
+    y_trajectory_newpath.append(newpath[i][1])
+
+fig, ax1 = plt.subplots(1, 1, figsize=(8, 8))
+ax1.plot(x_trajectory_path, y_trajectory_path, 'g', label='path')
+ax1.plot(x_trajectory_newpath, y_trajectory_newpath, 'r', label='newpath')
+
+plt.show()
